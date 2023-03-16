3f517a766f45c567ae2de8d6b394610f0fdfb93a
Tyler Mifflin <tylermifflin@Tylers-MacBook-Air.local>
2023-03-16 16:08:31 -0600

    I created more variables in my javascript to get elements by id and named off my ids in my html

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,6 @@
 var startquizbuttonEl = document.getElementById("startquizbutton");
 var timeEl = document.getElementById("time");
+var highscoreEl = document.getElementById("highscore");
+var jsmasterEl = document.getElementById("jsmaster");
+var questionsEl = document.getElementById("questions");
+var answersEl = document.getElementById("answers");
