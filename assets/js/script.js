38853315c6d6231a19f3f5d17eb2e4e5fbba0eaa
Tyler Mifflin <tylermifflin@Tylers-MacBook-Air.local>
2023-03-16 16:26:01 -0600

    added a separte variable for each question so they won't all be unhidden at once

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,10 @@ var startquizbuttonEl = document.getElementById("startquizbutton");
 var timeEl = document.getElementById("time");
 var highscoreEl = document.getElementById("highscore");
 var jsmasterEl = document.getElementById("jsmaster");
-var questionsEl = document.getElementById("questions");
+var question1El = document.getElementById("question1");
+var question2El = document.getElementById("question2");
+var question3El = document.getElementById("question3");
+var question4El = document.getElementById("question4");
 var answersEl = document.getElementById("answers");
 var resultsEl = document.getElementById("results");
 
@@ -11,6 +14,6 @@ startquizbuttonEl.addEventListener("click", startQuiz);
 function startQuiz() {
   startquizbuttonEl.classList.add("hide");
   jsmasterEl.classList.add("hide");
-  questionsEl.classList.remove("hide");
+  question1El.classList.remove("hide");
   setTime();
 }
