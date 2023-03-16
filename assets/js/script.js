fd9321a59f1181c69223339562e7e92f86238957
Tyler Mifflin <tylermifflin@Tylers-MacBook-Air.local>
2023-03-16 16:24:08 -0600

    trying to figure out the event listener to connect start button to quiz questions and timer, thinking i may need to make separate id for each question to not unhide them all at once

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,3 +5,12 @@ var jsmasterEl = document.getElementById("jsmaster");
 var questionsEl = document.getElementById("questions");
 var answersEl = document.getElementById("answers");
 var resultsEl = document.getElementById("results");
+
+startquizbuttonEl.addEventListener("click", startQuiz);
+//how to start the quiz and the timer once the start button is clicked?
+function startQuiz() {
+  startquizbuttonEl.classList.add("hide");
+  jsmasterEl.classList.add("hide");
+  questionsEl.classList.remove("hide");
+  setTime();
+}
