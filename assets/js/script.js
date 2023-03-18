ace38c1409bdf830646d37ec92e7f26edf40cc3a
Tyler Mifflin <tylermifflin@Tylers-MacBook-Air.local>
2023-03-18 15:08:04 -0600

    adding my anser variables and creating if statements to hide question 1 if clicked and also to change the time if the wrong answer is clicked

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,18 +11,31 @@ var resultsEl = document.getElementById("results");
 
 var timer = "";
 var results = "0, 25%, 50%, 75%, 100%";
+var answer question 1 = Boolean;
+var answer question 2 = Or; 
+var answer question 3 = Equal;
+var answer question 4 = ++;
 
 
 startquizbuttonEl.addEventListener("click", startQuizTimer);
 function startQuizTimer() {
+  var time = 60;
   timer = setInterval(function () {
-    if startquizbuttonEl = clicked {
-      timeEl.textContent = 60;
+    if (startquizbuttonEl === clicked) {
+      timeEl.textContent = time;
+      time--;
+     startquizbuttonEl.classList.add("hide");
+    jsmasterEl.classList.add("hide");
+    question1El.classList.remove("hide");
+    }
+    if (question1El.classList.remove("hide")=== clicked) {
+      question1El.classList.add("hide");
+      question2El.classList.remove("hide");
+      timeEl.textContent = time;
+      time--;
+    } else 
 
 
 
-  startquizbuttonEl.classList.add("hide");
-  jsmasterEl.classList.add("hide");
-  question1El.classList.remove("hide");
   
 }
