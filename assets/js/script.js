4afc7fb8af4151ce16afea18cef8c6f42093f49c
Tyler Mifflin <tylermifflin@Tylers-MacBook-Air.local>
2023-03-20 16:29:46 -0600

    changed up my else statements for my results to include all of the possible senarios for right and wrong answers to display results correctly

--- assets/js/script.js
+++ assets/js/script.js
@@ -25,7 +25,10 @@ function startquizbutton() {
      startquizbuttonEl.classList.add("hide");
     jsmasterEl.classList.add("hide");
     question1El.classList.remove("hide");
+    if (time === 0)
+      clearInterval(timer);
   }, 1000);
+}
     
     question1El.addEventListener("click", function(event) {
 
@@ -37,7 +40,7 @@ function startquizbutton() {
       question1El.classList.add("hide");
       question2El.classList.remove("hide");
     } 
-  });
+  }
     question2El.addEventListener("click", function(event) {
     if (event.target.textContent === question2El.children[1]) {
       question2El.classList.add("hide");
@@ -47,7 +50,7 @@ function startquizbutton() {
       question2El.classList.add("hide");
       question3El.classList.remove("hide");
     }
-  });
+  }
     question3El.addEventListener("click", function(event) {
     if (event.target.textContent === question3El.children[4]) {
       question3El.classList.add("hide");
@@ -57,9 +60,9 @@ function startquizbutton() {
       question3El.classList.add("hide");
       question4El.classList.remove("hide");
     }
-  });
+  }
     question4El.addEventListener("click", function(event) {
-    if (event.target.textContent === question4El.children{2}) {
+    if (event.target.textContent === question4El.children[2]) {
       question4El.classList.add("hide");
       resultsEl.classList.remove("hide");
     } else {timeEl.textContent = 0;
@@ -67,16 +70,15 @@ function startquizbutton() {
     }
   
     if (timeEl.textContent = 0 || question4El.classList.contains("hide")) {
-      clearInterval(timer);
       resultsEl.textContent = "You got " + results;
     }
     if (answerquestion1 === "boolean" && answerquestion2 === "0r" && answerquestion3 === "equal" && answerquestion4 === "++") {
       resultsEl.textContent = "You got " + results[4] + "!";
-    } else if (answerquestion1 === "boolean" && answerquestion2 === "or" && answerquestion3 === "equal") {
+    } else if (answerquestion1 === "boolean" && answerquestion2 === "or" && answerquestion3 === "equal" && answerquestion4 !== "++" || answerquestion1 === "boolean" && answerquestion2 === "or" && answerquestion3 !== "equal" && answerquestion4 === "++" || answerquestion1 === "boolean" && answerquestion2 !== "or" && answerquestion3 === "equal" && answerquestion4 === "++" || answerquestion1 !== "boolean" && answerquestion2 === "or" && answerquestion3 === "equal" && answerquestion4 === "++") {
       resultsEl.textContent = "You got " + results[3] + "!";
-    } else if (answerquestion1 === "boolean" && answerquestion2 === "or") {
+    } else if (answerquestion1 === "boolean" && answerquestion2 === "or" && answerquestion3 !== "equal" && answerquestion4 !== "++" || answerquestion1 === "boolean" && answerquestion2 !== "or" && answerquestion3 === "equal" && answerquestion4 !== "++" || answerquestion1 === "boolean" && answerquestion2 !== "or" && answerquestion3 !== "equal" && answerquestion4 === "++" || answerquestion1 !== "boolean" && answerquestion2 === "or" && answerquestion3 === "equal" && answerquestion4 !== "++" || answerquestion1 !== "boolean" && answerquestion2 === "or" && answerquestion3 !== "equal" && answerquestion4 === "++" || answerquestion1 !== "boolean" && answerquestion2 !== "or" && answerquestion3 === "equal" && answerquestion4 === "++") {
       resultsEl.textContent = "You got " + results[2] + "!";
-    } else if (answerquestion1 === "boolean") {
+    } else if (answerquestion1 === "boolean" && answerquestion2 !== "or" && answerquestion3 !== "equal" && answerquestion4 !== "++" || answerquestion1 !== "boolean" && answerquestion2 === "or" && answerquestion3 !== "equal" && answerquestion4 !== "++" || answerquestion1 !== "boolean" && answerquestion2 !== "or" && answerquestion3 === "equal" && answerquestion4 !== "++" || answerquestion1 !== "boolean" && answerquestion2 !== "or" && answerquestion3 !== "equal" && answerquestion4 === "++") {
       resultsEl.textContent = "You got " + results[1] + "!";
     } else {
       resultsEl.textContent = "You got " + results[0] + "!";
