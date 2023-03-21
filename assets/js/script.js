4ba48e63818673eccb7e4f846e2154684936dddc
Tyler Mifflin <tylermifflin@Tylers-MacBook-Air.local>
2023-03-20 18:24:37 -0600

    finally got my start button to work and actually display my first question

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,12 @@
-var startquizbuttonEl = document.querySelector('#startquizbutton');
-var timeEl = document.getElementById('time');
-var highscoreEl = document.getElementById('highscore');
-var jsmasterEl = document.getElementById('jsmaster');
-var question1El = document.getElementById('question1');
-var question2El = document.getElementById('question2');
-var question3El = document.getElementById('question3');
-var question4El = document.getElementById('question4');
-var resultsEl = document.getElementById('results');
+var startquizbuttonEl = document.getElementById("start-quiz-button");
+var timeEl = document.getElementById("time");
+var highscoreEl = document.getElementById("highscore");
+var jsmasterEl = document.getElementById("js-master");
+var question1El = document.getElementById("question1");
+var question2El = document.getElementById("question2");
+var question3El = document.getElementById("question3");
+var question4El = document.getElementById("question4");
+var resultsEl = document.getElementById("results");
 
 
 
@@ -14,7 +14,12 @@ var resultsEl = document.getElementById('results');
 startquizbuttonEl.addEventListener("click", function(event) {
   event.preventDefault();
   startquizbutton();
-}); 
+  startquizbuttonEl.classList.add(".hide");
+  if (event.target === startquizbuttonEl){
+    alert("test your knowledge of javascript");
+   question1El.setAttribute("style", "display: block");
+}
+});
  
 function startquizbutton() {
   var time = 60;
@@ -23,57 +28,56 @@ function startquizbutton() {
   var answerquestion2 = "or"; 
   var answerquestion3 = "equal";
   var answerquestion4 = "and";
-  timer = setInterval(function () {
-      timeEl.textContent = time;
-      time--;
-     startquizbuttonEl.classList.add("hide");
-    jsmasterEl.classList.add("hide");
-    question1El.classList.remove("hide");
-    if (time === 0)
-      clearInterval(timer);
+  var timerInterval = setInterval(function() {
+    time--;
+    timeEl.textContent = time;
+    if (time === 0) {
+      clearInterval(timerInterval);
+    }
+  
   }, 1000);
 }
 
     question1El.addEventListener("click", function(event) {
 
     if (event.target.textContent === question1El.children[2]) {
-      question1El.classList.add("hide");
-      question2El.classList.remove("hide");
+      question1El.classList.add(".hide");
+      question2El.classList.remove(".hide");
     } else {timeEl.textContent = 45;
       time--;
-      question1El.classList.add("hide");
-      question2El.classList.remove("hide");
+      question1El.classList.add(".hide");
+      question2El.classList.remove(".hide");
     } 
   });
     question2El.addEventListener("click", function(event) {
     if (event.target.textContent === question2El.children[1]) {
-      question2El.classList.add("hide");
-      question3El.classList.remove("hide");
+      question2El.classList.add(".hide");
+      question3El.classList.remove(".hide");
     }  else {timeEl.textContent = 30;
       time--;
-      question2El.classList.add("hide");
-      question3El.classList.remove("hide");
+      question2El.classList.add(".hide");
+      question3El.classList.remove(".hide");
     }
   });
     question3El.addEventListener("click", function(event) {
     if (event.target.textContent === question3El.children[4]) {
-      question3El.classList.add("hide");
-      question4El.classList.remove("hide");
+      question3El.classList.add(".hide");
+      question4El.classList.remove(".hide");
     } else {timeEl.textContent = 15;
       time--;
-      question3El.classList.add("hide");
-      question4El.classList.remove("hide");
+      question3El.classList.add(".hide");
+      question4El.classList.remove(".hide");
     }
   });
     question4El.addEventListener("click", function(event) {
     if (event.target.textContent === question4El.children[2]) {
-      question4El.classList.add("hide");
-      resultsEl.classList.remove("hide");
+      question4El.classList.add(".hide");
+      resultsEl.classList.remove(".hide");
     } else {timeEl.textContent = 0;
       time--;
     }
   
-    if (timeEl.textContent = 0 || question4El.classList.contains("hide")) {
+    if (timeEl.textContent = 0 || question4El.classList.contains(".hide")) {
       resultsEl.textContent = "You got " + results;
     }
     if (answerquestion1 === "boolean" && answerquestion2 === "0r" && answerquestion3 === "equal" && answerquestion4 === "++") {
@@ -88,3 +92,4 @@ function startquizbutton() {
       resultsEl.textContent = "You got " + results[0] + "!";
     }
   });
+
