5f5c94ca80956ad5010aca8267b190463a392ebb
Tyler Mifflin <tylermifflin@Tylers-MacBook-Air.local>
2023-03-22 15:25:02 -0600

    finally got my score to display correctly with my event listeners for each question

--- assets/js/script.js
+++ assets/js/script.js
@@ -32,9 +32,13 @@ function startquizbutton() {
   var timerInterval = setInterval(function() {
     time--;
     timeEl.textContent = time;
-    if (time === 0 || resultsEl.setAttribute("style", "display: block")) {
+    if (resultsEl.setAttribute("style", "display: block")) {
       clearInterval(timerInterval); }
   }, 1000);
+}
+function displayScore() {
+    resultsEl.setAttribute("style", "display: block")
+    resultsEl.textContent = + score + " out of 100";
 }
 
     question1El.addEventListener("click", function(event) {
@@ -44,16 +48,16 @@ function startquizbutton() {
       alert("correct")
       question1El.setAttribute("style", "display: none");
       question2El.setAttribute("style", "display: block");
-      score = score + scoreoptions[1];
-      displayScore();
+      score += scoreoptions[1];
+      
   
 
     } else if (event.target !==question1El.children[2]) {
       alert("wrong")
       question1El.setAttribute("style", "display: none");
       question2El.setAttribute("style", "display: block");
-      score = score + scoreoptions[0];
-      displayScore();
+      score += scoreoptions[0];
+      
       
 
     }
@@ -65,15 +69,15 @@ function startquizbutton() {
       alert("correct")
       question2El.setAttribute("style", "display: none");
       question3El.setAttribute("style", "display: block");
-      score = score + scoreoptions[1];
-      displayScore();
+      score += scoreoptions[1];
+      
       
     }  else if (event.target !==question2El.children[1]) {
       alert("wrong")
       question2El.setAttribute("style", "display: none");
       question3El.setAttribute("style", "display: block");
-      score = score + scoreoptions[0];
-      displayScore();
+      score += scoreoptions[0];
+      
 
       
     }
@@ -83,15 +87,15 @@ function startquizbutton() {
       alert("correct")
       question3El.setAttribute("style", "display: none");
       question4El.setAttribute("style", "display: block");
-      score = score + scoreoptions[1];
-      displayScore();
+      score += scoreoptions[1];
+      
       
     } else if (event.target !==question3El.children[4]) {
       alert("wrong")
       question3El.setAttribute("style", "display: none");
       question4El.setAttribute("style", "display: block");
-      score = score + scoreoptions[0];
-      displayScore();
+      score += scoreoptions[0];
+      
       
     }
   });
@@ -100,26 +104,16 @@ function startquizbutton() {
       alert("correct")
       question4El.setAttribute("style", "display: none");
       resultsEl.setAttribute("style", "display: block");
-      score = score + scoreoptions[1];
+      score += scoreoptions[1];
       displayScore();
 
     } else if (event.target !==question4El.children[2]) {
       alert("wrong")
       question4El.setAttribute("style", "display: none");
       resultsEl.setAttribute("style", "display: block");
-      score = score + scoreoptions[0];
+      score += scoreoptions[0];
       displayScore();
     }
     
-    function displayScore() {
-    if (time === 0 || resultsEl.setAttribute("style", "display: block")) {
-      resultsEl.textContent = "Your score is " + score + " out of 100";
-
-    }
     
   });
-  
-
-
-
-
